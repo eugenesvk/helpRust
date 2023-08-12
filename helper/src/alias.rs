0318ef92fd79d9e35fdfffd6b18913dabbb7985e
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-12 18:19:26 +0700

    add build print macro alias

--- helper/src/alias.rs
+++ helper/src/alias.rs
@@ -3,3 +3,6 @@ use crate::helper::*;
 pub use std          	::println as p; // requires text editor's syntax theme override to retain syntax highlighting
 pub use std::any     	::type_name; // for type_of
 pub use crate::helper	::print_type_of as pt;
+
+#[macro_export] macro_rules! pb { // println! during build
+  ($($tokens:tt)*) => {println!("cargo:warning={}", format!($($tokens)*))}}
