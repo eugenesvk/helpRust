847938850efd9d1b763a402db57ec37ac53100a1
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-12 17:37:58 +0700

    __update add helper
    allows using helper functions in build script

---
+++ helper/src/alias.rs
@@ -0,0 +1,5 @@
+// pub use reexports for use in other files
+use crate::helper::*;
+pub use std          	::println as p; // requires text editor's syntax theme override to retain syntax highlighting
+pub use std::any     	::type_name; // for type_of
+pub use crate::helper	::print_type_of as pt;
