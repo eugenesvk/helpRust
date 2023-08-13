b060f7495e0c1df3f0f0904572dde8c056d25587
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-14 01:06:02 +0700

    upd misc

--- src/misc.rs
+++ src/misc.rs
@@ -1,3 +1,17 @@
-// for loop with an index
-const looper = [1,2,3]
-for (i,val) in looper.iter().enumerate() {println!("looper i=¦{i}¦ vl=¦{val}¦")};
+fn ttt() {
+  // for loop with an index
+  const looper = [1,2,3]
+  for (i,val) in looper.iter().enumerate() {println!("looper i=¦{i}¦ vl=¦{val}¦")};
+}
+
+fn tt() {
+  key2bit.iter().find(|&&f) f == key_seq_user_def)
+  let desired = "control";
+  let to_eat = Ctrl.iter().find(|&&f| f == desired); //Some("control")
+  p!("{:?}", to_eat);
+  let desired = "ctrl1";
+  let to_eat = Ctrl.iter().find(|&&f| f == desired); //None
+  p!("{:?}", to_eat);
+  p!("¦{}¦", "⎈".to_lowercase()); //⎈
+
+}
