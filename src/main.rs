9541dc811c20afb3dc660baf20b2ca671c2458eb
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-13 15:34:50 +0700

    update allow list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros)]
+#![allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros,redundant_semicolons)]
 use helper::alias 	::*;
 use helper::helper	::*;
 
