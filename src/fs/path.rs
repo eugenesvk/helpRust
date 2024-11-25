3aab7a0b5d74009a860f553b612f91837e5bc1e2
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-25 19:42:49 +0700

    update path

--- src/fs/path.rs
+++ src/fs/path.rs
@@ -10,3 +10,17 @@ fn paths() {
   let from_combo	= Path::new("/etc").join("passwd");
   p!("{}", from_combo.display())
 }
+
+fn list_pahs<P: AsRef<Path>>(paths:&[P]) { // for args: use generic to accept Path,PathBuf,&str,Strings
+  // &[P] accepts Vec due to deref coercions
+  /* Rust Path vs PathBuf
+  - Path   	≈&str   or &[]	reference to the path string data but doesn't own this data (immutable, can't outlive actual data)
+  - PathBuf	≈String or Vec	owns the string data itself. Allocation
+  For arguments: use generic ↑ or
+    - PathBuf if you need to store it somewhere
+    - Path otherwise
+  For return types:
+    - Path: if return subpath of arg, e.g., Path[Buf].parent()
+    - PathBuf: If create a new path/combine paths
+  */
+}
