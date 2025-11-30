6d683df408d21b870effc23c13de9e80b7382055
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-01 01:22:42 +0700

    add symbols valid for identifiers

---
+++ src/syntax.rs
@@ -0,0 +1,11 @@
+/// Trick Rust into using visually descriptive symbols
+pub fn rust_syntax() {
+  let move𐎓 = 1;
+  let move𑁱 = 1;
+  let move𐊁 = 1;
+  let move𖭩 = 1;
+  let move𖭪 = 1;
+  let sym_ⵦ𐌭𐎓__𑁱𒐀𐎚𒐀__𐋇𐊁𐌮ⵃ𒾦𒾠___𐎂𐏑𐏒𒑖𐊵𐊜𐌞___𖣫𖭩𖭪𖭫𖭬 = 1;𐏐
+  let sym_bad_syntax_highlight___𐏔𐏓__𑁱 = 1; 𐄽
+  // : ⵦ𐌭𐎓𐏓𐏔 𑁱𒐀𐎚𒐀 𐋇𐌮ⵃ𒾦𒾠 𐎂𐏑𐏒𒑖𐊵𐊜𐌞 𖣫𖭩𖭪𖭫𖭬𐅁𐅀 too wide? weird spacing𐺉𐺆𐺁 𐺣 a weird space𐳄𐳅  𐤹𐤿 𐱀  no font 𐥉𐥗𐥑
+}
