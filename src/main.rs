6a26d1249d5057dda997987d2af2215009b8682b
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-01 01:22:25 +0700

    update lint rules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros,redundant_semicolons)]
+#![cfg_attr(not(debug_assertions),allow(uncommon_codepoints,non_snake_case,non_upper_case_globals,non_camel_case_types))]
+#![cfg_attr(    debug_assertions ,allow(uncommon_codepoints,non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros))]
 use helper::alias 	::*;
 use helper::helper	::*;
 use proc_macro    	::*;
